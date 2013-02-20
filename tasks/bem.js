d1b1ee3c9250f8489f774e7232abe51e45cf2db2
Anton Eprev <a.eprev@gmail.com>
2013-02-20 17:13:31 +0400

    Resolve require bem to the gruntfile

--- tasks/bem.js
+++ tasks/bem.js
@@ -10,15 +10,22 @@
 
 module.exports = function (grunt) {
 
+    var resolve = require('path').resolve.bind(process.cwd()),
+        requireResolve = function (module) {
+            var start = module.substr(0, 2);
+            return (start !== './' && start !== '..') ? module : resolve(module);
+        },
+        extend = grunt.util._.extend;
+
     grunt.registerMultiTask('bem', 'bem make', function () {
 
-        var options = grunt.util._.extend({
+        var options = extend({
                 require: 'bem',
                 targets: this.target
             }, this.options(), this.data),
             done = this.async();
 
-        var BEM = require(options.require),
+        var BEM = require(requireResolve(options.require)),
             Q = BEM.require('q');
 
         Q.when(BEM.api.make(options), done, function (err) {
